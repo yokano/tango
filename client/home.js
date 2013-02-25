386da59ee666d9971052736bbe5f3242a62cae2b
Yuta Okano <yuta.okano@gmail.com>
2013-02-25 10:26:12 +0900

    単語を追加したら画面を更新

--- client/home.js
+++ client/home.js
@@ -4,20 +4,26 @@ $(function() {
 	
 	// 追加ボタンを押したら単語を追加
 	addButton.bind('click', function() {
+	
+		// 入力チェック
+		if(word.val() == '') {
+			return;
+		}
+		
+		// サーバへ送信
 		$.ajax({
 			url: '/add',
 			data: {
 				word: word.val()
 			},
+			async: false,
 			success: function() {
-				console.log('完成');
+				$('ul').html($('ul').html() + ', ' + word.val());
+				word.val('');
 			},
 			error: function() {
 				console.log('ERROR');
 			}
 		});
 	});
-	
-	// 単語一覧の表示
-	
 });
